b865a5144d7bed583a0a2eb721ef7a7a08ed5ea2
Dmitry Shikov <shikovspace@yandex.ru>
2025-01-12 19:49:56 +0300

    added allure

---
+++ src/main/java/practicum/model/AuthorizationRequest.java
@@ -0,0 +1,19 @@
+package practicum.model;
+
+public class AuthorizationRequest {
+    private final String email;
+    private final String password;
+
+    public AuthorizationRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/test/java/practicum/DriverRule.java
@@ -0,0 +1,30 @@
+package practicum;
+
+import org.junit.rules.ExternalResource;
+import org.openqa.selenium.WebDriver;
+import practicum.drivers.WebDriverFactory;
+
+import java.time.Duration;
+
+public class DriverRule extends ExternalResource {
+
+    private WebDriver webDriver;
+
+    // Чтение передаваемого параметра browser (-Dbrowser)
+    String env = System.getProperty("browser", "chrome");
+
+    @Override
+    protected void before() {
+        webDriver = WebDriverFactory.getDriver(env.toLowerCase());
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    public WebDriver getWebDriver() {
+        return webDriver;
+    }
+
+    @Override
+    protected void after() {
+        webDriver.quit();
+    }
+}
